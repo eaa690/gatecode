f5407c2b1d9cd0b990aa664045f94b9634cbe6cf
Brian Michael <bsmichae42@yahoo.com>
2022-02-26 08:28:17 -0500

    Initial checkin

---
+++ src/main/java/org/eaa690/gatecode/GatecodeApplication.java
@@ -0,0 +1,13 @@
+package org.eaa690.gatecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatecodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/eaa690/gatecode/GatecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eaa690.gatecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
